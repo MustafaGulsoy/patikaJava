19d4fccfb53d590b4f2a6220b4f1fc44bfe23d81
Mustafa Gülsoy <mustafa30045@gmail.com>
2023-02-06 22:31:53 +0300

    Initial commit

---
+++ src/main/java/com/UltimaStella/patika/PatikaApplication.java
@@ -0,0 +1,13 @@
+package com.UltimaStella.patika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatikaApplication.class, args);
+	}
+
+}
